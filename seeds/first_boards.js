401e7728f875771d9f897c3168fc7d37fc625212
Austin Chan <auscwork@gmail.com>
2014-12-19 23:49:25 -0800

    forgot to add board files

---
+++ seeds/first_boards.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.seed = function(knex, Promise) {
+	return Promise.all([
+		knex('boards').insert([
+			{ name: 'politics', color: '93D74E' },
+			{ name: 'music', color: 'FC5E00' },
+			{ name: 'literature', color: 'FFDC1E' },
+			{ name: 'tv', color: 'B70FD9' }
+		]),
+		knex('entries').update({
+			board_id: 1
+		})
+	]);
+};
